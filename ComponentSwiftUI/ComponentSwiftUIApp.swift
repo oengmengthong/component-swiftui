22529c43c1a9f49c4be3c1196eaf3296d2afc128
Oeng Mengthong <mengthongoenggl@gmail.com>
2024-11-05 21:20:52 +0700

    Initial Commit

---
+++ ComponentSwiftUI/ComponentSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentSwiftUIApp.swift
+//  ComponentSwiftUI
+//
+//  Created by Mengthong JakSmok on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ComponentSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComponentSwiftUITests/ComponentSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  ComponentSwiftUITests.swift
+//  ComponentSwiftUITests
+//
+//  Created by Mengthong JakSmok on 5/11/24.
+//
+
+import Testing
+
+struct ComponentSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
